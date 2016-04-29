3709e33fd6a63f3946f72a011b7456be9814c9ba
dandisy <dandi.webdev@gmail.com>
2016-04-29 23:42:29 +0700

    Starting WEBCORE development remove database configuration, add webcore config and webcore helper

---
+++ application/config/webcore.php
@@ -0,0 +1,5 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config['asset_path'] = 'uploads';
+
+$config['image_path'] = 'http://dev.redbuzz.net/indonesiakaya/html/uploads';
